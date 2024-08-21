63794ca3c4438d57a125a30d17e6a40edbb1f5c4
inf-sup <174466778+inf-sup@users.noreply.github.com>
2024-08-21 12:31:42 +0800

    feat: update qbittorrent
    4.6.5 -> 5.0.0-rc1

--- prepare/workdir/build.sh
+++ prepare/workdir/build.sh
@@ -1,7 +1,16 @@
+# max build time 2h
+sleep 7200 && kill -SIGKILL $$ &
+# install packages
+install_pkg=$(realpath "./install_pkg.sh")
+include_pkg=''
+exclude_pkg=''
+bash $install_pkg -i -d $(realpath 'linglong/sources') -p $PREFIX -I \"$include_pkg\" -E \"$exclude_pkg\"
+export LD_LIBRARY_PATH=$PREFIX/lib/$TRIPLET:$LD_LIBRARY_PATH
+
 # build ninja
 cd /project/linglong/sources/ninja.git
 cmake -Bbuild
-cmake --build build
+cmake --build build -j$(nproc)
 ninja="$(pwd)/build/ninja"
 
 # build libtorrent
@@ -12,8 +21,10 @@ cmake -Bbuild \
     -DCMAKE_CXX_STANDARD=17 \
     -DCMAKE_BUILD_TYPE=Release \
     -Ddeprecated-functions=OFF \
-    -DCMAKE_INSTALL_PREFIX=$PREFIX
-cmake --build build
+    -DCMAKE_INSTALL_PREFIX=build/install \
+    -DCMAKE_INSTALL_LIBDIR=$PREFIX/lib/$TRIPLET \
+    -DCMAKE_INSTALL_INCLUDEDIR=$PREFIX/include
+cmake --build build -j1
 cmake --install build
 
 # build qBittorrent
@@ -24,5 +35,16 @@ cmake -Bbuild \
     -DCMAKE_CXX_STANDARD=17 \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=$PREFIX
-cmake --build build
+cmake --build build -j1
 cmake --install build
+
+# fix QT_PLUGIN_PATH
+{
+  echo "[Paths]"
+  echo "Prefix=$PREFIX"
+  echo "Plugins=lib/${TRIPLET}/qt6/plugins"
+} > ${PREFIX}/bin/qt.conf
+
+rm -r $PREFIX/include
+# uninstall dev packages
+bash $install_pkg -u -r '\-dev|tools|qmake6'
